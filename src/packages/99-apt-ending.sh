8890cede7693e38ab319c50801e96768c886a06f
Vladimir Vaskov <rirusha@altlinux.org>
2025-08-25 14:48:33 +0300

    ci: Add dated image

--- src/packages/99-apt-ending.sh
+++ src/packages/99-apt-ending.sh
@@ -2,14 +2,10 @@
 
 echo "::group:: ===$(basename "$0")==="
 
-# Создаем новый каталог для базы rpm
-mkdir -p /usr/share/rpm
-rsync -aA /var/lib/rpm/ /usr/share/rpm/
-rm -rf /var/lib/rpm && ln -s ../../usr/share/rpm /var/lib/rpm
-
 apt-get remove -y -D apt-repo
 apt-get autoremove -y
 apt-get clean
+
 rm -rf /var/lib/apt/lists/*
 mkdir /var/lib/apt/lists/partial
 
