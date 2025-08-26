fac9b461823504c66153b399c552482e35aefc9c
Vladimir Vaskov <rirusha@altlinux.org>
2025-08-27 01:47:11 +0300

    doc(kernel): Change comments

--- src/configuration/03-kernel.sh
+++ src/configuration/03-kernel.sh
@@ -6,30 +6,13 @@ echo "::group:: ===$(basename "$0")==="
 KERNEL_DIR="/usr/lib/modules"
 
 echo "Detecting kernel version..."
+# uname is not an option
 KERNEL_VERSION=$(ls "$KERNEL_DIR" | head -n 1)
 
 if [[ -z "$KERNEL_VERSION" ]]; then
     echo "Error: No kernel version found in $KERNEL_DIR."
     exit 1
 fi
-
-# DRIVERS=$(find "${KERNEL_DIR}/${KERNEL_VERSION}/kernel/drivers" \( \
-#         -path "${KERNEL_DIR}/${KERNEL_VERSION}/kernel/drivers/hid/*" \
-#         -o -path "${KERNEL_DIR}/${KERNEL_VERSION}/kernel/drivers/gpu/*" \
-#         -o -path "${KERNEL_DIR}/${KERNEL_VERSION}/kernel/drivers/pci/*" \
-#         -o -path "${KERNEL_DIR}/${KERNEL_VERSION}/kernel/drivers/mmc/*" \
-#         -o -path "${KERNEL_DIR}/${KERNEL_VERSION}/kernel/drivers/usb/host/*" \
-#         -o -path "${KERNEL_DIR}/${KERNEL_VERSION}/kernel/drivers/usb/storage/*" \
-#         -o -path "${KERNEL_DIR}/${KERNEL_VERSION}/kernel/drivers/nvmem/*" \
-#         -o -path "${KERNEL_DIR}/${KERNEL_VERSION}/kernel/drivers/nvme/*" \
-#         -o -path "${KERNEL_DIR}/${KERNEL_VERSION}/kernel/drivers/virtio/*" \
-#         -o -path "${KERNEL_DIR}/${KERNEL_VERSION}/kernel/drivers/video/fbdev/*" \
-#     \) -type f -name '*.ko*' | sed 's:.*/::')
-
-# cat << EOF > /usr/lib/dracut/dracut.conf.d/50_kmdir.conf
-# hostonly=no
-# add_drivers+=" $DRIVERS "
-# EOF
 
 # TODO: Package file
 UPDATE_INITRAMFS_FILE=/usr/libexec/update-initramfs
