a78e61c6fc89f040c196618554d8aca76ab6069a
Vladimir Vaskov <rirusha@altlinux.org>
2025-09-09 23:22:32 +0300

    tasks: Add testing new version ostree

--- src/packages/03-tasks-list.sh
+++ src/packages/03-tasks-list.sh
@@ -3,7 +3,7 @@
 echo "::group:: ===$(basename "$0")==="
 
 TASKS=(
-
+    394485
 )
 
 for task in "${TASKS[@]}"; do 
@@ -13,7 +13,8 @@ done
 apt-get update
 
 TASKS_PACKAGES=(
-    
+    libostree
+    ostree
 )
 
 if [ ${#TASKS_PACKAGES[@]} -gt 0 ]; then
