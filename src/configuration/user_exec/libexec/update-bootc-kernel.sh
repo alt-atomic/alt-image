5389f1322b070bca2651fe40b102ac15ebe26d63
Роман Меркушин <rmerkushin@yandex.ru>
2025-10-04 20:49:07 +0300

    Added the ability to update the initramfs and vmlinuz of any version

---
+++ src/configuration/user_exec/libexec/update-bootc-kernel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "Updating the initramfs and vmlinuz..."
+
+KERNEL_DIR="/usr/lib/modules"
+
+echo "Detecting kernel version..."
+# uname is not an option
+KERNEL_VERSION=$(ls "$KERNEL_DIR" | head -n 1)
+
+if [[ -z "$KERNEL_VERSION" ]]; then
+    echo "Error: No kernel version found in $KERNEL_DIR."
+    exit 1
+fi
+
+# Depmod and autoload
+depmod -a -v "${KERNEL_VERSION}"
+
+# Rebuilding initramfs
+dracut --force "$KERNEL_DIR/$KERNEL_VERSION/initramfs.img" $KERNEL_VERSION
+
+# Copy vmlinuz for bootc
+cp -f "/boot/vmlinuz-$KERNEL_VERSION" "$KERNEL_DIR/$KERNEL_VERSION/vmlinuz"
